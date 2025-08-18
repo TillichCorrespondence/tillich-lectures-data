99f3ab661a245e0b97c9c07e8bec7fcb8b49cb5d
csae8092 <p.andorfer@gmail.com>
2025-08-18 11:33:10 +0200

    wip: convert custom keyword tags to proper rs tags

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import glob
+
 from acdh_tei_pyutils.tei import TeiReader
+
+to_change = [
+    "religion",
+    "ultimate_concern",
+    "unconscious",
+    "biology",
+    "monistic_theory",
+    "genesis",
+    "language",
+    "community",
+    "church",
+    "nation",
+    "individualism",
+]
 
 
 def main():
@@ -7,7 +22,12 @@ def main():
     files = glob.glob("data/editions/*.xml")
     for x in sorted(files):
         doc = TeiReader(x)
-        print(doc.any_xpath(".//tei:title[@type='main']")[0].text)
+        for y in to_change:
+            rs_type = "_".join([y.capitalize() for y in y.split("_")])
+            for el in doc.any_xpath(f"tei:{y}"):
+                el.tag = "{http://www.tei-c.org/ns/1.0}rs"
+                el.attrib["type"] = "keyword"
+                el.attrib["type"] = f"#{rs_type}"
 
 
 if __name__ == "__main__":
